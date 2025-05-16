4a79a573b23150faf34702ea75e7eff3f873b0cd
Lea Carnibella <leandrocarnibella@gmail.com>
2025-05-16 10:52:04 -0300

    Update successful-login.cy.js

--- cypress-typescript-e2e/successful-login.cy.js
+++ cypress-typescript-e2e/successful-login.cy.js
@@ -1,1 +1,13 @@
+//Test login con credenciales correctas.
+describe('Login - OrangeHRM Demo', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/');
+  });
+  it('Debería loguearse correctamente con credenciales válidas', () => {
+    cy.get('input[name="username"]').type('Admin'); 
+    cy.get('input[name="password"]').type('admin123');
+    cy.get('button[type="submit"]').click();
+    cy.url().should('include', '/dashboard');
+    cy.wait(3000);
+  });
+
 
